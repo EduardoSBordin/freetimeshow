56c4a827b078c105b7ad1ca49a2576e67eaee2cd
Eduardo Bordin <dudubordin26@gmail.com>
2024-09-22 02:07:59 -0300

    v4

--- fts/src/components/pages/Home.jsx
+++ fts/src/components/pages/Home.jsx
@@ -6,13 +6,24 @@ import '../styles/Home.css'
 function Home(){
 
     return(
-        <div>
+        <div className='mainHome'>
 
     <Navbar />
-    <section id='home'></section>
+    <section id='home'>
+        <div className="contentHome">
+            <div className="textHome">
+                <h1>Os Simpsons</h1>
+                <p>The Simpsons é uma série animada norte americana transmitida
+                     pelo canal FOX. A comédia acompanha a família Simpsons,
+                      uma família da classe trabalhadora na cidade desajustada
+                       de Springfield. Homer, o pai, trabalha como inspetor de
+                        segurança na usina nuclear local; Marge, a mãe, tenta 
+                        manter a paz na família.</p>
+            </div>
+        </div>
+    </section>
  
     <section id='cartoon'>
-    <CartoonsHome />
     <CartoonsHome />
     </section>
         </div>
